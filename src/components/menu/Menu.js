4fc0c00c4cb3e51e39b0760096002a9bd59b0d54
walwizard <goneisgone@hotmail.com>
2023-01-22 10:47:42 -0300

    Pré navbar end

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -4,18 +4,19 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Menu = () => {
   return (
-    <div className="navbar">
+    <div className="nav">
       <img
         src={require("../assets/logo.png")}
         alt="logo"
         className="logo__img"
       />
-      <ul className="navbar__itens">
-        <li className="navbar__item">home</li>
-        <li className="navbar__item">product</li>
-        <li className="navbar__item">promo</li>
-        <li className="navbar__item">about</li>
-        <li className="navbar__item">contact</li>
+
+      <ul className="navbar">
+        <li className="navbar__itens">home</li>
+        <li className="navbar__itens">product</li>
+        <li className="navbar__itens">promo</li>
+        <li className="navbar__itens">about</li>
+        <li className="navbar__itens">contact</li>
       </ul>
     </div>
   );
