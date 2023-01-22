4aa2a4079af6cc32f209ba72530289e24ca686de
walwizard <goneisgone@hotmail.com>
2023-01-22 16:55:18 -0300

    Atualizações

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './App.css';
-import Menu from './components/menu/Menu';
+import React from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from './components/main/Main';
 
+import Main from "./components/main/Main";
+import Menu from "./components/menu/Menu";
 
 function App() {
   return (
     <div className="App">
-        <Menu />
+      <Menu />
 
+      <BrowserRouter>
         <Routes>
-            <Route exact path="/"  element={ <Main />}/>
+          <Route exact path="/" element={<Main />} />
         </Routes>
+      </BrowserRouter>
     </div>
   );
 }
