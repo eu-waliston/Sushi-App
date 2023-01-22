6be70efa660dc03fb93216a84036a5d9208189b0
walwizard <goneisgone@hotmail.com>
2023-01-22 12:31:00 -0300

    Rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-
 import Menu from './components/menu/Menu';
-import Main from './components/main/Main';
+
+import Rotas from './routes';
+
 
 function App() {
   return (
     <div className="App">
         <Menu />
-        <Main />
+        <Rotas />
     </div>
   );
 }
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from '../components/main/Main'
+
+const MainScreen = () => {
+    return (
+        <Main />
+    )
+}
+
+export default MainScreen;
