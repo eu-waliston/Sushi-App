64585b4af103ef0da7f1e4ad1975bc06214e9293
walwizard <goneisgone@hotmail.com>
2023-01-29 23:48:53 -0300

    hosting

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIuEbwIb5PAy7aSpo0jR2Y1LS8cePyZBs",
+  authDomain: "sushi-app-986f7.firebaseapp.com",
+  projectId: "sushi-app-986f7",
+  storageBucket: "sushi-app-986f7.appspot.com",
+  messagingSenderId: "87975999338",
+  appId: "1:87975999338:web:e00f1c6911be1562645713"
+};
+
+
+const app = initializeApp(firebaseConfig);
