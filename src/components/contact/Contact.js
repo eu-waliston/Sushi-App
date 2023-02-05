8078cba34e95eba0b2b014de0d0d742e825435a6
walwizard <goneisgone@hotmail.com>
2023-02-05 14:02:18 -0300

    Adjusts

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from "react";
 import './Contact.css';
 
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,55 +1,54 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-import './Menu.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Link } from "react-router-dom";
+import './Menu.css'
 
 const Menu = () => {
 
-  const [isNavExpanded, setIsNavExpanded] = React.useState('nav');
+  const [isNavExpanded, setIsNavExpanded] = React.useState("nav");
 
   return (
-    <div className={isNavExpanded ? "nav" : "nav__resposive"}>
-      <i className="bi bi-list"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded);
-        }}
-      ></i>
-      <img
-        src={require("../assets/logo.png")}
-        alt="logo"
-        className="logo__img"
-      />
+    <Fragment>
+      <i className="bi bi-list" onClick={() => { setIsNavExpanded(!isNavExpanded) }}></i>
+      <div className={isNavExpanded ? "nav" : "nav__resposive"}>
+        <img
+          src={require("../assets/logo.png")}
+          alt="logo"
+          className="logo__img"
+        />
 
-      <ul className="navbar">
-        <li className="navbar__itens">
-          <Link to={"/"} className="navbar_LK">
-            home
-          </Link>
-        </li>
-        <li className="navbar__itens">
-          <Link to={"/product"} className="navbar_LK">
-            products
-          </Link>
-        </li>
 
-        <li className="navbar__itens">
-          <Link to={"/promo"} className="navbar_LK">
-            promo
-          </Link>
-        </li>
-        <li className="navbar__itens">
-          <Link to={"/about"} className="navbar_LK">
-            about
-          </Link>
-        </li>
-        <li className="navbar__itens">
-          <Link to={"/contact"} className="navbar_LK">
-            contact
-          </Link>
-        </li>
-      </ul>
-    </div>
+        <ul className="navbar">
+          <li className="navbar__itens">
+            <Link to={"/"} className="navbar_LK">
+              home
+            </Link>
+          </li>
+          <li className="navbar__itens">
+            <Link to={"/product"} className="navbar_LK">
+              products
+            </Link>
+          </li>
+
+          <li className="navbar__itens">
+            <Link to={"/promo"} className="navbar_LK">
+              promo
+            </Link>
+          </li>
+          <li className="navbar__itens">
+            <Link to={"/about"} className="navbar_LK">
+              about
+            </Link>
+          </li>
+          <li className="navbar__itens">
+            <Link to={"/contact"} className="navbar_LK">
+              contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Fragment>
   );
 };
 
